78322a1d9b852cb5cf0a0d8da980a2413e04fbc4
paypas <adearmawi@gmail.com>
2019-08-16 14:44:41 +0700

    School case:

---
+++ src/Economic.java
@@ -0,0 +1,9 @@
+public class Economic extends Course{
+
+	Economic(int courseTime, String courseCode, String courseName) {
+		super(courseTime, courseCode, courseName);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
---
+++ src/Math.java
@@ -0,0 +1,10 @@
+
+public class Math extends Course{
+
+	Math(int courseTime, String courseCode, String courseName) {
+		super(courseTime, courseCode, courseName);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
